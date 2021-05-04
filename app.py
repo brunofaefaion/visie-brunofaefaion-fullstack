109f54b08acb3531bcdfbe3846cda75b21a159fa
brunofaefaion <bruno.fae.faion@gmail.com>
2021-05-03 21:56:04 -0300

    fist api route implementation and serving index.html

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+app.config['MYSQL_HOST'] = 'jobs.visie.com.br'
+app.config['MYSQL_USER'] = 'brunofaion'
+app.config['MYSQL_PASSWORD'] = 'YnJ1bm9mYWlv'
+app.config['MYSQL_DB'] = 'brunofaion'
+
+mysql = MySQL(app)
+
+class Pessoas(Resource):
+    def get(self):
+        # cursor = mysql.connection.cursor()
+        # result = cursor.execute(''' SELECT * FROM pessoas ''')
+        # mysql.connection.commit()
+        # data = cursor.fetchall()
+        # cursor.close()
+        return {"msg": "hello world!"}
+
+api.add_resource(Pessoas, '/api/pessoas')
+
+@app.route('/')
+def home():
+   return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
